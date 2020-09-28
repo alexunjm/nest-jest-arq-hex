cdd80b577dde03bd601d81d3ceffcc573fd158f3
alexanderjaramillo <alexander.jaramillo@ceiba.com.co>
2020-09-28 15:39:16 +0000

    nueva prueba en clase para convertir Date a Fecha - se agrega validación para caso null en contructor - se agrega validación para caso undefined en contructor

--- test/unitarias/dominio/fecha/fecha-desde-instancia-date.spec.ts
+++ test/unitarias/dominio/fecha/fecha-desde-instancia-date.spec.ts
@@ -15,9 +15,27 @@ describe('RangoFechas', () => {
     };
   });
 
-  it('una fecha Date (de js) debería convertirse en una intancia de _Fecha', () => {
-    const today = new Date();
-    expect(new _FechaDesdeInstanciaDate(today)).toBeInstanceOf(_Fecha);
+  it('un objeto Date (de js) debería convertirse en una intancia de Fecha', () => {
+    let hoy: Date;
+    hoy = new Date();
+    expect(new _FechaDesdeInstanciaDate(hoy)).toBeInstanceOf(_Fecha);
+  });
+
+  it([
+    'un objeto Date (de js) null o undefined NO debería poder crear una objeto',
+    'tipo fecha y debería lanzar un error'
+  ].join(' '), () => {
+    let unObjetoDate: Date;
+    const fechaDateComoNull = () => {
+      unObjetoDate = null;
+      new _FechaDesdeInstanciaDate(unObjetoDate);
+    }
+    expect(fechaDateComoNull).toThrowError(/^null/);
+    const fechaDateComoUndefined = () => {
+      unObjetoDate = undefined;
+      new _FechaDesdeInstanciaDate(unObjetoDate);
+    }
+    expect(fechaDateComoUndefined).toThrowError(/^undefined/);
   });
 
 });
