4f286f0df4be5bf7aa20078255949514a484044d
alexanderjaramillo <alexander.jaramillo@ceiba.com.co>
2020-09-28 16:19:18 +0000

    cambio en Fecha - cambia de class a interface

--- src/dominio/fecha/modelo/fecha.ts
+++ src/dominio/fecha/modelo/fecha.ts
@@ -1,32 +1,8 @@
-export class Fecha {
+export interface Fecha extends DatosFecha {
+    
+    esMenorQue(fecha: Fecha): boolean;
 
-    anio: number;
-    mes: number;
-    dia: number;
+    esIgualA(fecha: Fecha): boolean;
 
-    constructor(datosFecha: DatosFecha) {
-        this.anio = datosFecha.anio;
-        this.mes = datosFecha.mes;
-        this.dia = datosFecha.dia;
-    }
-
-    esMenorQue(fecha: Fecha): any {
-        return !this.esIgualA(fecha) &&
-            this.anio <= fecha.anio &&
-            this.mes <= fecha.mes &&
-            this.dia <= fecha.dia;
-    }
-
-    esIgualA(fecha: Fecha) {
-        return this.anio === fecha.anio &&
-            this.mes === fecha.mes &&
-            this.dia === fecha.dia;
-    }
-
-    esMayorQue(fecha: Fecha): any {
-        return !this.esIgualA(fecha) &&
-            this.anio >= fecha.anio &&
-            this.mes >= fecha.mes &&
-            this.dia >= fecha.dia;
-    }
+    esMayorQue(fecha: Fecha): boolean;
 }
