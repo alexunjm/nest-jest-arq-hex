78e5c90a0f763de818ae54f8020ed4985b72c323
alexanderjaramillo <alexander.jaramillo@ceiba.com.co>
2020-09-28 15:38:11 +0000

    mejora en renombrado de variable tipo Date

--- src/dominio/fecha/modelo/fecha-desde-instancia-date.ts
+++ src/dominio/fecha/modelo/fecha-desde-instancia-date.ts
@@ -1,17 +1,21 @@
+import { ErrorValorRequerido } from "src/dominio/errores/error-valor-requerido";
 import { Fecha } from "./fecha";
 
 export class FechaDesdeInstanciaDate extends Fecha {
 
     fechaTipoDate: Date;
 
-    constructor(unaFechaTipoDate: Date) {
-        super(FechaDesdeInstanciaDate.datosFechaDe(unaFechaTipoDate));
-        this.fechaTipoDate = unaFechaTipoDate;
+    constructor(objetoDate: Date) {
+        super(FechaDesdeInstanciaDate.datosFechaDe(objetoDate));
+        this.fechaTipoDate = objetoDate;
     }
-    static datosFechaDe(unaFecha: Date): DatosFecha {
-        const anio = unaFecha.getFullYear();
-        const mes = unaFecha.getMonth();
-        const dia = unaFecha.getDate();
+
+    private static datosFechaDe(objetoDate: Date): DatosFecha {
+        if (!objetoDate) throw new ErrorValorRequerido(`${objetoDate} no es una fecha válida`);
+        
+        const anio = objetoDate.getFullYear();
+        const mes = objetoDate.getMonth();
+        const dia = objetoDate.getDate();
         return { anio, mes, dia };
     }
 }
