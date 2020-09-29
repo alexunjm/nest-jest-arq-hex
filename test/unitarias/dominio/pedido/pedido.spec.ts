6ca4678bdd4bc847746d1fa58effc05589822e31
alexanderjaramillo <alexander.jaramillo@ceiba.com.co>
2020-09-29 16:23:32 +0000

    se quitan las pruebas que necesitaban mocks - se quitan del archivo spec las pruebas - se quitan del modelo del dominio pedido las funciones que no le corresponden

--- test/unitarias/dominio/pedido/pedido.spec.ts
+++ test/unitarias/dominio/pedido/pedido.spec.ts
@@ -20,33 +20,4 @@ describe('Pedido', () => {
         expect(unPedido.fechaPago).toBeUndefined();
         expect(unPedido.fechaPago).not.toBeNull();
     });
-
-    it('3. al marcar como pagado un pedido, debe tener fecha de pago', () => {
-
-        unPedido.marcarComoPagado();
-        expect(unPedido.fechaPago).not.toBeUndefined();
-        expect(unPedido.fechaPago).not.toBeNull();
-        expect(unPedido.fechaPago).toBeInstanceOf(Date);
-    });
-
-    it([
-        '4. un pedido sin marcar como pagado, debe indicar que NO fue pagado',
-        'entre la fecha en que se tomó el pedido y la fecha actual'
-    ].join(' '), () => {
-
-        const fechaActual = new Date();
-        const resultado = unPedido.fuePagadoEnRangoDeFechas(unPedido.fecha, fechaActual);
-        expect(resultado).toBe(false);
-    });
-
-    it([
-        '5. un pedido con fecha de pago, debe indicar que fue pagado',
-        'entre la fecha en que se tomó el pedido y la fecha actual'
-    ].join(' '), () => {
-
-        unPedido.fechaPago = new Date();
-        const fechaActual = new Date();
-        const resultado = unPedido.fuePagadoEnRangoDeFechas(unPedido.fecha, fechaActual);
-        expect(resultado).toBe(true);
-    });
 });
